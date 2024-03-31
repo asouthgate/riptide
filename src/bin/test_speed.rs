c4c3d5e7bce1f7a216946f6320c0ca3fbd9f328f
Alex Southgate <southgateJA@cardiff.ac.uk>
2024-03-31 10:29:05 +0100

    use fluidstate interface in run-time test instead of ad hoc

--- src/bin/test_speed.rs
+++ src/bin/test_speed.rs
@@ -10,21 +10,16 @@ fn main() {
     let pg = PixelGrid::new(m, n);
     let mut fs = FluidState::new(pg.m, pg.n);
     let ps = JacobiPressureSolver {};
-    let mut ak;
     let iterations = 10;
-    let start_time = Instant::now(); // Start the timer
+    let start_time = Instant::now();
     for _it in 1..iterations {
-        for i in 1..m {
-            for j in 1..n {
-                ak = i * n + j;
-                cal_new_velocity_boundary_aware_no_diffusion(&mut fs, &pg, ak, 1.0);
-            }
-        }
+        fs.momentum_step(&pg, 1.0);
+        fs.cal_divergence(&pg);
         ps.solve(&mut fs, &pg, 5);
-        fs.swap_vectors();
+        fs.apply_corrections();
     }
 
-    let end_time = Instant::now(); // End the timer
+    let end_time = Instant::now();
 
     let elapsed_time = end_time - start_time; // Calculate the elapsed time
     let avg_time_per_iteration = elapsed_time / iterations as u32; // Calculate average time per iteration
