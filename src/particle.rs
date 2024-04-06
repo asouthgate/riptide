830cb23f35af3bfe3a883de09ca9920b52dcfc5b
Alex Southgate <southgateJA@cardiff.ac.uk>
2024-04-06 19:46:32 +0100

    reformulate particle physics simulation to use forces

--- src/particle.rs
+++ src/particle.rs
@@ -6,6 +6,8 @@ pub struct Particle {
     pub position: (f32, f32),
     pub velocity: (f32, f32),
     pub acceleration: (f32, f32),
+    pub f_hydro: (f32, f32),
+    pub f_drag: (f32, f32),
     pub mass: f32,
     pub trail: Vec<(f32, f32)>,
     pub trail_length: usize,
@@ -17,10 +19,7 @@ pub struct Particle {
 impl Particle {
     pub fn new(x: f32, y: f32) -> Self {
         Particle {
-            position: (x, y), velocity: (0.0, 0.0),
-            acceleration: (0.0, 0.0), mass: 1.0,
-            trail: vec![], trail_length: 0, t: 0.0,
-            lifespan: 0.0, rgba: (0, 0, 0, 0)
+            position: (x, y), mass: 1.0, ..Default::default()
         }
     }
     pub fn is_dead(&self) -> bool{
@@ -40,44 +39,95 @@ impl Particle {
     }
 }
 
-struct RigidBody {
+pub fn length(u: f32, v: f32) -> f32 {
+    (u.powf(2.0) + v.powf(2.0)).powf(0.5)
+}
+
+#[derive(Default)]
+pub struct RigidBody {
     virtual_particles: Vec<Particle>,
+    central_particle: Particle,
+    relative_positions: Vec<(f32, f32)>,
 }
 
 impl RigidBody {
-    pub fn new(positions: Vec<(f32, f32)>) -> Self {
+    pub fn new(x0: f32, y0: f32, mass_density: f32, positions: Vec<(f32, f32)>) -> Self {
         let mut vps = vec![];
-        for (x, y) in positions {
+        for (x, y) in &positions {
             vps.push(Particle {
-                position: (x, y), velocity: (0.0, 0.0),
-                acceleration: (0.0, 0.0), mass: 1.0,
-                trail: vec![], trail_length: 0, t: 0.0,
-                lifespan: 0.0, rgba: (0, 0, 0, 0)
+                position: (x0 + *x, y0 + *y), mass: mass_density,
+                ..Default::default()
             });
         }
+        let n_particles = positions.len();
         RigidBody {
-            virtual_particles: vps
+            virtual_particles: vps,
+            central_particle: Particle { position: (x0, y0), mass: mass_density, ..Default::default() },
+            relative_positions: positions,
         }
     }
 
     pub fn update_derivatives(&mut self, fs: &FluidState, pg: &PixelGrid, dt: f32) {
-        let mut central_acceleration = (0.0, 0.0);
-        let mut central_velocity = (0.0, 0.0);
         let mut c = 0;
+        self.central_particle.f_hydro = (0.0, 0.0);
+        self.central_particle.f_drag = (0.0, 0.0);
         for p in &mut self.virtual_particles {
-            update_particle_derivatives(fs, pg, p, dt);
-            central_acceleration.0 += p.acceleration.0;   
-            central_acceleration.1 += p.acceleration.1;           
-            central_velocity.0 += p.velocity.0;   
-            central_velocity.1 += p.velocity.1;
+            // println!(
+            //     "\t??? {} {} -> {} {}", p.velocity.0, p.velocity.1, p.f_drag.0, p.f_drag.1
+            // );
+            update_fluid_forces(fs, pg, p, dt);
+            self.central_particle.f_hydro.0 += p.f_hydro.0;
+            self.central_particle.f_hydro.1 += p.f_hydro.1;
+            self.central_particle.f_drag.0 += p.f_drag.0;
+            self.central_particle.f_drag.1 += p.f_drag.1;
+            // println!(
+            //     "\t({:.5} {:.5}) | ph ({:.5} {:.5}) pd ({:.5} {:.5}) a ({:.5} {:.5}) v ({:.5} {:.5}) |v| {:.5}",
+            //     p.position.0, p.position.1,
+            //     p.f_hydro.0, p.f_hydro.1,
+            //     p.f_drag.0, p.f_drag.1,
+            //     p.acceleration.0, p.acceleration.1,
+            //     p.velocity.0, p.velocity.1,
+            //     length(p.velocity.0, p.velocity.1)
+            // );
             c += 1;
         }
-        let mut total_mass = self.virtual_particles.len() as f32;
+        self.central_particle.f_hydro.0 /= self.virtual_particles.len() as f32;
+        self.central_particle.f_hydro.1 /= self.virtual_particles.len() as f32;
+        self.central_particle.f_drag.0 /= self.virtual_particles.len() as f32;
+        self.central_particle.f_drag.1 /= self.virtual_particles.len() as f32;
+
+        update_particle_derivatives(fs, pg, &mut self.central_particle, dt);
+
+        // println!(
+        //     "({:.5} {:.5}) | ph ({:.5} {:.5}) pd ({:.5} {:.5}) a ({:.5} {:.5}) v ({:.5} {:.5})",
+        //     self.central_particle.position.0, self.central_particle.position.1,
+        //     self.central_particle.f_hydro.0, self.central_particle.f_hydro.1,
+        //     self.central_particle.f_drag.0, self.central_particle.f_drag.1,
+        //     self.central_particle.acceleration.0, self.central_particle.acceleration.1,
+        //     self.central_particle.velocity.0, self.central_particle.velocity.1,
+        // );
+
         for p in &mut self.virtual_particles {
-            p.acceleration.0 = central_acceleration.0 / total_mass;
-            p.acceleration.1 = central_acceleration.1 / total_mass;
-            p.velocity.0 = central_velocity.0 / total_mass;
-            p.velocity.1 = central_velocity.1 / total_mass;
+            p.acceleration.0 = self.central_particle.acceleration.0;
+            p.acceleration.1 = self.central_particle.acceleration.1;
+            p.velocity.0 = self.central_particle.velocity.0;
+            p.velocity.1 = self.central_particle.velocity.1;
+            // println!(
+            //     "\t({:.5} {:.5}) | ({:.5} {:.5}) ({:.5} {:.5}) ({:.5} {:.5})",
+            //     p.position.0, p.position.1,
+            //     p.f_hydro.0, p.f_hydro.1,
+            //     p.acceleration.0, p.acceleration.1,
+            //     p.velocity.0, p.velocity.1,
+            // );
+        }
+    }
+
+    pub fn update_child_positions(&mut self) {
+        let mut c = 0;
+        for position in &self.relative_positions {
+            self.virtual_particles[c].position.0 = self.central_particle.position.0 + position.0;
+            self.virtual_particles[c].position.1 = self.central_particle.position.1 + position.1;
+            c += 1;
         }
     }
 
@@ -96,19 +146,35 @@ impl RigidBody {
             }
             c += 1;
         }
-        // finally, update them all
-        for particle in &mut self.virtual_particles {
-            particle.position.0 += dx;
-            particle.position.1 += dy;
-        }
+        self.central_particle.position.0 += dx;
+        self.central_particle.position.1 += dy;
+        self.update_child_positions();
     }
+
     pub fn evolve(&mut self, fs: &FluidState, pg: &PixelGrid, dt: f32) {
         self.update_derivatives(fs, pg, dt);
         self.update_position(fs, pg, dt);
     }
+
+    pub fn get_x(&mut self) -> &mut f32 {
+        &mut self.central_particle.position.0
+    }
+
+    pub fn get_y(&mut self) -> &mut f32 {
+        &mut self.central_particle.position.1
+    }
+
+    pub fn get_u(&mut self) -> &mut f32 {
+        &mut self.central_particle.velocity.0
+    }
+
+    pub fn get_v(&mut self) -> &mut f32 {
+        &mut self.central_particle.velocity.1
+    }
+
 }
 
-fn update_particle_derivatives(fs: &FluidState, pg: &PixelGrid, p: &mut Particle, dt: f32) {
+fn update_fluid_forces(fs: &FluidState, pg: &PixelGrid, p: &mut Particle, dt: f32) {
     // fluid forces, for unit density and unit area, is just velocity
     // this can be calculated from the momentum flux
     // or, imagine water is little balls
@@ -118,11 +184,19 @@ fn update_particle_derivatives(fs: &FluidState, pg: &PixelGrid, p: &mut Particle
     // however, what's also important is _relative_ velocity
     // since if our object is going the same velocity as the flow
     // there will be no momentum flux
-    let fx_hydro = pg.sample_bilinear(&fs.u, p.get_x(), p.get_y()) - p.get_u();
-    let fy_hydro = pg.sample_bilinear(&fs.v, p.get_x(), p.get_y()) - p.get_v();
+    let fu = pg.sample_bilinear_world(&fs.u, p.get_x(), p.get_y());
+    let fv = pg.sample_bilinear_world(&fs.v, p.get_x(), p.get_y());
+    p.f_hydro = (fu, fv);
+    let cdrag = 0.01;
+    // f_drag is 
+    p.f_drag = (-p.get_u() * cdrag, -p.get_v() * cdrag);
+}
 
-    p.acceleration.0 += dt * fx_hydro / p.mass; // makes sense, the higher the mass the lower the acceleration
-    p.acceleration.1 += dt * fy_hydro / p.mass;
+fn update_particle_derivatives(fs: &FluidState, pg: &PixelGrid, p: &mut Particle, dt: f32) {
+    p.acceleration.0 = p.f_hydro.0 / p.mass; // makes sense, the higher the mass the lower the acceleration
+    p.acceleration.1 = p.f_hydro.1 / p.mass;
+    p.acceleration.0 += p.f_drag.0 / p.mass;
+    p.acceleration.1 += p.f_drag.1 / p.mass;
     p.velocity.0 += p.acceleration.0 * dt;
     p.velocity.1 += p.acceleration.1 * dt;
 }
@@ -169,6 +243,7 @@ fn update_particle_position(
 }
 
 fn evolve_particle(fs: &FluidState, pg: &PixelGrid, p: &mut Particle, dt: f32) {
+    update_fluid_forces(fs, pg, p, dt);
     update_particle_derivatives(fs, pg, p, dt);
     update_particle_position(fs, pg, p, dt);
     if p.trail.len() > p.trail_length {
@@ -184,6 +259,7 @@ fn evolve_particle(fs: &FluidState, pg: &PixelGrid, p: &mut Particle, dt: f32) {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::prelude::*;
 
     #[test]
     fn test_evolve_rigidbody() {
@@ -203,18 +279,13 @@ mod tests {
         }
         // first test a single particle, it should be the same as Particle
         let mut pos1 = vec![];
-        pos1.push((1.0, 1.0));
-        let mut rb1 = RigidBody::new(pos1);
+        pos1.push((0.0, 0.0));
+        let mut rb1 = RigidBody::new(1.0, 1.0, 1.0, pos1);
         let mut p = Particle {
             position: (1.0, 1.0),
-            velocity: (0.0,  0.0),
-            acceleration: (0.0, 0.0),
             mass: 1.0,
-            trail: vec![],
-            trail_length: 0,
-            t: 0.0,
             lifespan: 4.0,
-            rgba: (0, 0, 0, 0)        
+            ..Default::default()
         };
         for _it in 0..6 {
             evolve_particle(&fs, &pg, &mut p, 1.0);
@@ -222,25 +293,26 @@ mod tests {
         }
         assert!(p.position.0 == 7.0); // started at 1, 1, did 6 steps
         assert!(p.position.1 == 4.0);
-        assert!(rb1.virtual_particles[0].get_x() == 7.0);
-        assert!(rb1.virtual_particles[0].get_y() == 4.0);
+        assert!(*rb1.get_x() == p.position.0);
+        assert!(*rb1.get_y() == p.position.1);
 
         let mut pos2 = vec![];
-        pos2.push((1.0, 1.0));
-        pos2.push((1.0, 2.0));
-        let mut rb2 = RigidBody::new(pos2);
+        pos2.push((0.0, 0.0));
+        pos2.push((0.0, 1.0));
+        let mut rb2 = RigidBody::new(1.0, 1.0, 1.0, pos2);
         pg.print_data(&fs.boundary);
         for _it in 0..10 {
-            rb1.evolve(&fs, &pg, 1.0);
+            rb2.evolve(&fs, &pg, 1.0);
         }
 
         assert!(rb2.virtual_particles[0].get_y() == rb2.virtual_particles[1].get_y() - 1.0);
+        assert!(rb2.virtual_particles[0].get_y() == *rb2.get_y());
+        assert!(rb2.virtual_particles[0].get_x() == *rb2.get_x());
 
         assert!(rb2.virtual_particles[0].get_x() < 8.0);
         assert!(rb2.virtual_particles[0].get_y() < 7.0);
         assert!(rb2.virtual_particles[1].get_x() < 8.0);
         assert!(rb2.virtual_particles[1].get_y() < 8.0);
-
     }
 
     #[test]
@@ -261,14 +333,9 @@ mod tests {
         }
         let mut p = Particle {
             position: (1.0, 1.0),
-            velocity: (0.0,  0.0),
-            acceleration: (0.0, 0.0),
             mass: 1.0,
-            trail: vec![],
-            trail_length: 0,
-            t: 0.0,
             lifespan: 4.0,
-            rgba: (0, 0, 0, 0)        
+            ..Default::default()
         };
         pg.print_data(&fs.u);
         pg.print_data(&fs.v);
