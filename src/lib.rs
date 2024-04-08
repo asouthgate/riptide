e14d2d7234c274f52cd649735cea4a1ec82b61d6
Alex Southgate <southgateJA@cardiff.ac.uk>
2024-04-08 12:09:25 +0100

    implement SPH for particles

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 pub mod boundary;
+pub mod fluid_macroscopic_features;
 pub mod fluid_state;
 pub mod momentum;
 pub mod particle;
 pub mod pixelgrid;
 pub mod pressure;
-pub mod fluid_macroscopic_features;
+pub mod sph;
